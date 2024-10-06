61601932bcff6829d2f6d5bcc3e3512fb9921e4d
ehabyanni <ehabayanni@gmail.com>
2024-10-06 21:00:58 +0300

    Implement skillDetailsSlice and inject in the store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import jobsReducer from './slices/jobSlice';
 import searchReducer from './slices/searchSlice';
 import jobDetailsReducer from './slices/jobDetailsSlice';
+import skillDetailsReducer from './slices/skillDetailsSlice';
 
 const store = configureStore({
   reducer: {
     jobs: jobsReducer,
     search: searchReducer,
     jobDetails: jobDetailsReducer,
+    skillDetails: skillDetailsReducer
   }
 });
 
---
+++ src/store/slices/skillDetailsSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, createAsyncThunk, SerializedError } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { jobDetailsType } from '../../models/jobDetailsType';
+import { SkillDetailsType } from '../../models/skillDetailsType';
+
+// Fetch specific Skill details by ID
+export const fetchSkillDetails = createAsyncThunk(
+  'skills/fetchSkillDetails',
+  async (id: string) => {
+    const response = await axios.get(`https://skills-api-zeta.vercel.app/skill/${id}`);
+    return response.data;
+  }
+);
+
+const skillDetailsSlice = createSlice({
+  name: 'skills',
+  initialState: {
+    skillDetails: {} as SkillDetailsType,
+    status: 'idle',
+    error: null as SerializedError | null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSkillDetails.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchSkillDetails.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.skillDetails = action.payload.data.skill;
+      })
+      .addCase(fetchSkillDetails.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error;
+      });
+  }
+});
+
+export default skillDetailsSlice.reducer;
