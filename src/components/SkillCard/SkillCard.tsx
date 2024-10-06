fc157cfcc5edcd1081a60d3c024370a5ed8211b0
ehabyanni <ehabayanni@gmail.com>
2024-10-06 23:11:46 +0300

    navigate to skill page

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,12 +1,14 @@
 'use client'
 import './style.scss';
 import { SkillDetailsType } from '../../models/skillDetailsType';
+import { useNavigate } from 'react-router-dom';
 
 function SkillCard({ skill }: { skill: SkillDetailsType }) {
+    const navigate = useNavigate();
 
     return (
         <div id="skillCard">
-            <h1>{skill?.attributes?.name}</h1>
+            <h1 onClick={() => navigate(`/skill/${skill.id}`)}>{skill?.attributes?.name}</h1>
             {/* <h3>{skill?.attributes?.}</h3> */}
             <div id="skillsInfo">
                 <div id="skillType">
