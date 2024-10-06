907199f1993730df095e8c3daaa83ec02bf72ebd
ehabyanni <ehabayanni@gmail.com>
2024-10-06 13:12:15 +0300

    Refactor Skill card to recieve one skill

--- src/components/SkillCard/SkillCard.tsx
+++ src/components/SkillCard/SkillCard.tsx
@@ -1,43 +1,23 @@
 'use client'
-import { useEffect, useState } from 'react';
 import './style.scss';
-import { Skill, SkillCardProps } from '../../models/skill';
+import { SkillDetailsType } from '../../models/skillDetailsType';
 
-function SkillCard({ skills }: SkillCardProps) {
-
-    const [skillsArray, setSkillsArray] = useState<Skill[]>([]);
-
-    useEffect(() => {
-        setSkillsArray(skills);
-    }, [skills]);
+function SkillCard({ skill }: { skill: SkillDetailsType }) {
 
     return (
-        <div id="skillCardOuterContainer">
-            <div id="skillCardHeader">
-                <h1>Related Skills:</h1>
-            </div>
-            <div id="skillCardContainer">
-                {
-                    skillsArray?.map((skill: Skill) => {
-                        return (
-                            <div id="skillCard" key={skill?.id}>
-                                <h1>{skill?.title}</h1>
-                                <h3>{skill?.desc}</h3>
-                                <div id="skillsInfo">
-                                    <div id="skillType">
-                                        <p><b>Type:</b> {skill?.type}</p>
-                                    </div>
-                                    <div id="skillImportance">
-                                        <p><b>Importance:</b> {skill?.importance}</p>
-                                    </div>
-                                    <div id="skillLevel">
-                                        <p><b>Level:</b> {skill?.level}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+        <div id="skillCard">
+            <h1>{skill?.attributes?.name}</h1>
+            {/* <h3>{skill?.attributes?.}</h3> */}
+            <div id="skillsInfo">
+                <div id="skillType">
+                    <p><b>Type:</b> {skill?.attributes?.type}</p>
+                </div>
+                <div id="skillImportance">
+                    <p><b>Importance:</b> {skill?.attributes?.importance}</p>
+                </div>
+                <div id="skillLevel">
+                    <p><b>Level:</b> {skill?.attributes?.level}</p>
+                </div>
             </div>
         </div>
     )
