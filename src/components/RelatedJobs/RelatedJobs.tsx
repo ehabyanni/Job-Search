7db34be3f25eff7a6e7cae88d8d3685851ce1cfb
ehabyanni <ehabayanni@gmail.com>
2024-10-06 23:13:13 +0300

    Add SkillType params bollean to differeniate  between skill / job and handle some conditions depends on that is the same code.

--- src/components/RelatedJobs/RelatedJobs.tsx
+++ src/components/RelatedJobs/RelatedJobs.tsx
@@ -5,10 +5,11 @@ import { useNavigate } from 'react-router-dom';
 import { fetchJobs } from '../../store/slices/jobSlice';
 
 interface skillsIDsProps {
-    skillsIDs: {id : string}[];
+    skillsIDs: { id: string }[];
+    skillType?: boolean;
 }
 
-function RelatedJobs({ skillsIDs }: skillsIDsProps) {
+function RelatedJobs({ skillsIDs, skillType = false }: skillsIDsProps) {
 
     const [relatedJobs, setRelatedJobs] = useState<any[]>([]);
     const navigate = useNavigate();
@@ -33,25 +34,39 @@ function RelatedJobs({ skillsIDs }: skillsIDsProps) {
             );
             setRelatedJobs(matchedJobs);
         }
-    }, [skillsIDs]);
+    }, [skillsIDs , jobs]);
 
     const handleQueryClick = (id: string) => {
-        navigate(`/jobs/${id}`);
+        skillType ? navigate(`/skill/${id}`) : navigate(`/jobs/${id}`);
     };
 
     return (
         <div id='relatedJobs'>
             <div id="relatedJobsContainer">
-                <h3>Related Jobs:</h3>
-                <ul>
-                    {relatedJobs.map((job, index) => (
-                        <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                            <li key={index} onClick={() => handleQueryClick(job.id)} style={{ cursor: 'pointer' }}>
-                                {job.attributes.title}
-                            </li>
-                        </div>
-                    ))}
-                </ul>
+                <h3>{skillType ? 'Related Skills' : 'Related Jobs'}</h3>
+                {
+                    !skillType ? (
+                        <ul>
+                            {relatedJobs?.map((job, index) => (
+                                <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                    <li key={index} onClick={() => handleQueryClick(job.id)} style={{ cursor: 'pointer' }}>
+                                        {job.attributes.title}
+                                    </li>
+                                </div>
+                            ))}
+                        </ul>
+                    ) : (
+                        <ul>
+                            {skillsIDs?.map((skill, index) => (
+                                <div key={index} style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                                    <li key={index} onClick={() => handleQueryClick(skill.id)} style={{ cursor: 'pointer' }}>
+                                        {skill.id}
+                                    </li>
+                                </div>
+                            ))}
+                        </ul>
+                    )
+                }
             </div>
         </div>
     );
