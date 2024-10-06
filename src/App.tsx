559d523852402bcdbc27bb0a5ae027bf24808159
ehabyanni <ehabayanni@gmail.com>
2024-10-06 18:09:16 +0300

    add skillDetails route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import History from './pages/History';
 import Home from './pages/Home/Home';
 import JobDetails from './pages/JobDetails/JobDetails';
 import Search from './pages/Search/Search';
+import SkillDetails from './pages/SkillDetails/SkillDetails';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/jobs/search" element={<Search />} />
           <Route path="/history" element={<History />} />
           <Route path="/jobs/:id" element={<JobDetails />} />
+          <Route path="/skill/:id" element={<SkillDetails />} />
           {/* <Route path="*" element={<NoMatch />} /> */}
         </Routes>
       </Layout>
